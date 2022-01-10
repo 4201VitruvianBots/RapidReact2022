403ff7aeea02c31e5aefb10f103f94ccbd62faae
Natalie <nmwpenguin@gmail.com>
2022-01-10 10:45:52 -0800

    Put in values for 2 ball auto - still needs work

--- src/main/java/frc/robot/commands/auto/OneBallAuto.java
+++ src/main/java/frc/robot/commands/auto/OneBallAuto.java
@@ -1,44 +1,140 @@
-// Copyright (c) FIRST and other WPILib contributors.
-// Open Source Software; you can modify and/or share it under the terms of
-// the WPILib BSD license file in the root directory of this project.
+package frc.robot.commands.autonomous.routines;
 
-package frc.robot.commands.auto;
+import edu.wpi.first.wpilibj.RobotBase;
+import edu.wpi.first.wpilibj.RobotController;
+import edu.wpi.first.wpilibj.Timer;
+import edu.wpi.first.wpilibj.geometry.Pose2d;
+import edu.wpi.first.wpilibj.geometry.Rotation2d;
+import edu.wpi.first.wpilibj.geometry.Transform2d;
+import edu.wpi.first.wpilibj.geometry.Translation2d;
+import edu.wpi.first.wpilibj.shuffleboard.Shuffleboard;
+import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
+import edu.wpi.first.wpilibj.smartdashboard.SmartDashboardTab;
+import edu.wpi.first.wpilibj.trajectory.Trajectory;
+import edu.wpi.first.wpilibj.trajectory.TrajectoryConfig;
+import edu.wpi.first.wpilibj.trajectory.TrajectoryGenerator;
+import edu.wpi.first.wpilibj.trajectory.constraint.CentripetalAccelerationConstraint;
+import edu.wpi.first.wpilibj.trajectory.constraint.DifferentialDriveKinematicsConstraint;
+import edu.wpi.first.wpilibj.trajectory.constraint.DifferentialDriveVoltageConstraint;
+import edu.wpi.first.wpilibj.util.Units;
+import edu.wpi.first.wpilibj2.command.ParallelDeadlineGroup;
+import edu.wpi.first.wpilibj2.command.PrintCommand;
+import edu.wpi.first.wpilibj2.command.SequentialCommandGroup;
+import edu.wpi.first.wpilibj2.command.WaitCommand;
+import frc.robot.RobotContainer;
+import frc.robot.commands.autonomous.SmartdashboardCommand;
+import frc.robot.commands.autonomous.TurnInPlace;
+import frc.robot.commands.drivetrain.SetDriveNeutralMode;
+import frc.robot.commands.drivetrain.SetDriveShifters;
+import frc.robot.commands.drivetrain.SetOdometry;
+import frc.robot.commands.intake.SetIntakeSpeed;
+import frc.robot.commands.intake.SetIntakePiston;
+import frc.robot.commands.intake.SetIntakeStates;
+import frc.robot.constants.Constants;
+import frc.robot.simulation.FieldSim;
+import frc.robot.simulation.SimulationShoot;
+import frc.robot.subsystems.*;
+import frc.vitruvianlib.utils.TrajectoryUtils;
+import java.util.ArrayList;
+import java.util.List;
 
-import frc.robot.subsystems.DriveTrain;
-import frc.robot.subsystems.ExampleSubsystem;
-import edu.wpi.first.wpilibj2.command.CommandBase;
+public class AutoNavBarrel extends SequentialCommandGroup {
+    public AutoNavBarrel(DriveTrain driveTrain, FieldSim fieldSim) {
+        int[][] waypointsRaw = { // x, y, and z coordinates of the robot (x and y in inches, z is degrees of
+                                 // rotation)
+                { 40, 90, 0 },
+                { 150, 90, 0 },
+                { 176, 45, -120 },
+                { 135, 45, 120 },
+                { 150, 90, 0 },
+                { 250, 96, 30 },
+                { 270, 141, 135 },
+                { 210, 120, -80 },
+                { 290, 45, -45 },
+                { 330, 45, 45 },
+                { 300, 92, 175 },
+                { 180, 102, 180 },
+                { 30, 102, 180 }
+        };
 
-/** Description shoot one cargo into the HUB then pick up one more cargo*/
-public class OneBallAuto extends CommandBase {
-    @SuppressWarnings({"PMD.UnusedPrivateField", "PMD.SingularField"})
-    private final DriveTrain m_driveTrain;
+        /*
+         * Runs through each of the set of x, y and z values in the list below one at a
+         * time.
+         * At each set (starting at 0), it merges the x and y values of that set into on
+         * Pose2d coordinate
+         * The varibale j is used to represent which set in the list is currently
+         * processing (starting at 0)
+         * The [0] and [1] specify the values being merged are from the x [0] and y [1]
+         * columns of the list.
+         */
 
-    /**
-     * One Ball Auto 
-     *
-     * @param driveTrain The subsystem used by this command.
-     */
-    public OneBallAuto(DriveTrain driveTrain) {
-        m_driveTrain = driveTrain;
-        // Use addRequirements() here to declare subsystem dependencies.
-        addRequirements(driveTrain);
-    }
+        Pose2d[] waypoints = new Pose2d[waypointsRaw.length];
+        for (int j = 0; j < waypointsRaw.length; j++) {
+            waypoints[j] = new Pose2d(Units.inchesToMeters(waypointsRaw[j][0]),
+                    Units.inchesToMeters(waypointsRaw[j][1]),
+                    new Rotation2d(Units.degreesToRadians(waypointsRaw[j][2])));
+        }
 
-    // Called when the command is initially scheduled.
-    @Override
-    public void initialize() {}
+        Pose2d startPosition = waypoints[0]; // starting point is the first set in the waypoints array
 
-    // Called every time the scheduler runs while the command is scheduled.
-    @Override
-    public void execute() {}
+        TrajectoryConfig configA = new TrajectoryConfig(Units.feetToMeters(10), Units.feetToMeters(6)); // 10 -
+                                                                                                        // acceleration,
+                                                                                                        // 6 -
+                                                                                                        // speed
+                                                                                                        // (m/s
+                                                                                                        // and
+                                                                                                        // m/s/s)
+        configA.setReversed(false); // 'true' would make it go the opposite direction
+        // configA.setEndVelocity(configA.getMaxVelocity());
+        configA.addConstraint(new DifferentialDriveKinematicsConstraint(driveTrain.getDriveTrainKinematics(),
+                configA.getMaxVelocity()));
+        configA.addConstraint(new DifferentialDriveVoltageConstraint(driveTrain.getFeedforward(),
+                driveTrain.getDriveTrainKinematics(), 10));
+        configA.addConstraint(new CentripetalAccelerationConstraint(1.7)); // This is what we can change when
+                                                                           // we're actually testing (turning
+                                                                           // speed)
 
-    // Called once the command ends or is interrupted.
-    @Override
-    public void end(boolean interrupted) {}
+        addCommands(new SetDriveShifters(driveTrain, Constants.DriveConstants.inSlowGear),
+                new SetOdometry(driveTrain, fieldSim, startPosition),
+                new SetDriveNeutralMode(driveTrain, 0));
 
-    // Returns true when the command should end.
-    @Override
-    public boolean isFinished() {
-        return false;
+        double[] startVelocities = {
+                0, // starting velocity from point 0 to point 1
+                2 * configA.getMaxVelocity() / 3, // starting velocity from point 1 to point 2
+                2 * configA.getMaxVelocity() / 3,
+                2 * configA.getMaxVelocity() / 3,
+                2 * configA.getMaxVelocity() / 3,
+                2 * configA.getMaxVelocity() / 3,
+                2 * configA.getMaxVelocity() / 3,
+                3 * configA.getMaxVelocity() / 4,
+                3 * configA.getMaxVelocity() / 4,
+                3 * configA.getMaxVelocity() / 4,
+                3 * configA.getMaxVelocity() / 4,
+                3 * configA.getMaxVelocity() / 4 };
+        double[] endVelocities = {
+                2 * configA.getMaxVelocity() / 3, // ending velocity at point 1 from point 0
+                2 * configA.getMaxVelocity() / 3, // ending velocity at point 2 from point 1
+                2 * configA.getMaxVelocity() / 3,
+                2 * configA.getMaxVelocity() / 3,
+                2 * configA.getMaxVelocity() / 3,
+                2 * configA.getMaxVelocity() / 3,
+                3 * configA.getMaxVelocity() / 4,
+                3 * configA.getMaxVelocity() / 4,
+                3 * configA.getMaxVelocity() / 4,
+                3 * configA.getMaxVelocity() / 4,
+                3 * configA.getMaxVelocity() / 4,
+                2 * configA.getMaxVelocity() / 3 };
+
+        for (int i = 0; i < waypoints.length - 1; i++) {
+            configA.setStartVelocity(startVelocities[i]);
+            configA.setEndVelocity(endVelocities[i]);
+
+            Trajectory trajectory = TrajectoryGenerator.generateTrajectory(waypoints[i],
+                    List.of(),
+                    waypoints[i + 1], // makes a path from one waypoint to the next
+                    configA); // makes sure that path fits under the constraints
+            var command = TrajectoryUtils.generateRamseteCommand(driveTrain, trajectory);
+            addCommands(command);
+        }
     }
 }
--- src/main/java/frc/robot/commands/auto/TwoBallAuto.java
+++ src/main/java/frc/robot/commands/auto/TwoBallAuto.java
@@ -1,51 +1,141 @@
-// Copyright (c) FIRST and other WPILib contributors.
-// Open Source Software; you can modify and/or share it under the terms of
-// the WPILib BSD license file in the root directory of this project.
-
 package frc.robot.commands.auto;
 
-import frc.robot.subsystems.DriveTrain;
-import edu.wpi.first.wpilibj2.command.CommandBase;
+import edu.wpi.first.wpilibj.RobotBase;
+import edu.wpi.first.wpilibj.RobotController;
+import edu.wpi.first.wpilibj.Timer;
+import edu.wpi.first.math.geometry.Pose2d;
+import edu.wpi.first.math.geometry.Rotation2d;
+import edu.wpi.first.math.geometry.Transform2d;
+import edu.wpi.first.math.geometry.Translation2d;
+// import edu.wpi.first.math.shuffleboard.Shuffleboard;
+import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
+import edu.wpi.first.wpilibj.smartdashboard.SmartDashboardTab;
+import edu.wpi.first.math.trajectory.Trajectory;
+import edu.wpi.first.math.trajectory.TrajectoryConfig;
+import edu.wpi.first.math.trajectory.TrajectoryGenerator;
+import edu.wpi.first.math.trajectory.constraint.CentripetalAccelerationConstraint;
+import edu.wpi.first.math.trajectory.constraint.DifferentialDriveKinematicsConstraint;
+import edu.wpi.first.math.trajectory.constraint.DifferentialDriveVoltageConstraint;
+import edu.wpi.first.math.util.Units;
+import edu.wpi.first.wpilibj2.command.ParallelDeadlineGroup;
+import edu.wpi.first.wpilibj2.command.PrintCommand;
+import edu.wpi.first.wpilibj2.command.SequentialCommandGroup;
+import edu.wpi.first.wpilibj2.command.WaitCommand;
+import frc.robot.RobotContainer;
+// import frc.robot.commands.auto.SmartdashboardCommand;
+// import frc.robot.commands.auto.TurnInPlace;
+import frc.robot.commands.driveTrain.SetDriveNeutralMode;
+//import frc.robot.commands.driveTrain.SetDriveShifters;
+import frc.robot.commands.driveTrain.SetOdometry;
+// import frc.robot.commands.intake.SetIntakeSpeed;
+// import frc.robot.commands.intake.SetIntakePiston;
+// import frc.robot.commands.intake.SetIntakeStates;
+import frc.robot.Constants;
+import frc.robot.simulation.FieldSim;
+import frc.robot.simulation.SimulationShoot;
+import frc.robot.subsystems.*;
+import frc.vitruvianlib.utils.TrajectoryUtils;
+import java.util.ArrayList;
+import java.util.List;
 
 /** intakes one cargo and intakes two cargo into the high goal */
-public class TwoBallAuto extends CommandBase {
-  @SuppressWarnings({ "PMD.UnusedPrivateField", "PMD.SingularField" })
-  private final DriveTrain m_driveTrain;
+public class TwoBallAuto extends SequentialCommandGroup {
+    /**
+     * Creates a new TwoBallAuto.
+     *
+     * @param driveTrain The driveTrain used by this command.
+     */
+    public TwoBallAuto(DriveTrain driveTrain, FieldSim fieldSim) {
+        // Drive backward maximum distance to ball
+        // While dirivng backward, intake is running
+        // Stop (now with 2 cargo) and aim for high goal
+        // Shoot 2 cargo into high goal
 
-  /**
-   * Creates a new TwoBallAuto.
-   *
-   * @param driveTrain The driveTrain used by this command.
-   */
-  public TwoBallAuto(DriveTrain driveTrain) {
-    m_driveTrain = driveTrain;
-    // Use addRequirements() here to declare subsystem dependencies.
-    addRequirements(driveTrain);
-  }
+        int[][] waypointsRaw = { // x, y, and z coordinates of the robot (x and y in inches, z is degrees of
+                                 // rotation)
+                { 0, 0, 0 },
+                { -120, 0, 0 },
 
-  // Called when the command is initially scheduled.
-  @Override
-  public void initialize() {
-  }
+        };
 
-  // Called every time the scheduler runs while the command is scheduled.
-  @Override
-  public void execute() {
-  }
+        /*
+         * Runs through each of the set of x, y and z values in the list below one at a
+         * time.
+         * At each set (starting at 0), it merges the x and y values of that set into on
+         * Pose2d coordinate
+         * The varibale j is used to represent which set in the list is currently
+         * processing (starting at 0)
+         * The [0] and [1] specify the values being merged are from the x [0] and y [1]
+         * columns of the list.
+         */
 
-  // Drive backward maximum distance to ball
-  // While dirivng backward, intake is running
-  // Stop (now with 2 cargo) and aim for high goal
-  // Shoot 2 cargo into high goal
+        Pose2d[] waypoints = new Pose2d[waypointsRaw.length];
+        for (int j = 0; j < waypointsRaw.length; j++) {
+            waypoints[j] = new Pose2d(Units.inchesToMeters(waypointsRaw[j][0]),
+                    Units.inchesToMeters(waypointsRaw[j][1]),
+                    new Rotation2d(Units.degreesToRadians(waypointsRaw[j][2])));
+        }
 
-  // Called once the command ends or is interrupted.
-  @Override
-  public void end(boolean interrupted) {
-  }
+        Pose2d startPosition = waypoints[0]; // starting point is the first set in the waypoints array
 
-  // Returns true when the command should end.
-  @Override
-  public boolean isFinished() {
-    return false;
-  }
+        TrajectoryConfig configA = new TrajectoryConfig(Units.feetToMeters(10), Units.feetToMeters(6)); // 10 -
+                                                                                                        // acceleration,
+                                                                                                        // 6 -
+                                                                                                        // speed
+                                                                                                        // (m/s
+                                                                                                        // and
+                                                                                                        // m/s/s)
+        configA.setReversed(false); // 'true' would make it go the opposite direction
+        // configA.setEndVelocity(configA.getMaxVelocity());
+        configA.addConstraint(new DifferentialDriveKinematicsConstraint(driveTrain.getDriveTrainKinematics(),
+                configA.getMaxVelocity()));
+        configA.addConstraint(new DifferentialDriveVoltageConstraint(driveTrain.getFeedforward(),
+                driveTrain.getDriveTrainKinematics(), 10));
+        configA.addConstraint(new CentripetalAccelerationConstraint(1.7)); // This is what we can change when
+                                                                           // we're actually testing (turning
+                                                                           // speed)
+
+        addCommands(
+                new SetOdometry(driveTrain, fieldSim, startPosition),
+                new SetDriveNeutralMode(driveTrain, 0));
+
+        double[] startVelocities = {
+                0, // starting velocity from point 0 to point 1
+                2 * configA.getMaxVelocity() / 3, // starting velocity from point 1 to point 2
+                2 * configA.getMaxVelocity() / 3,
+                2 * configA.getMaxVelocity() / 3,
+                2 * configA.getMaxVelocity() / 3,
+                2 * configA.getMaxVelocity() / 3,
+                2 * configA.getMaxVelocity() / 3,
+                3 * configA.getMaxVelocity() / 4,
+                3 * configA.getMaxVelocity() / 4,
+                3 * configA.getMaxVelocity() / 4,
+                3 * configA.getMaxVelocity() / 4,
+                3 * configA.getMaxVelocity() / 4 };
+        double[] endVelocities = {
+                2 * configA.getMaxVelocity() / 3, // ending velocity at point 1 from point 0
+                2 * configA.getMaxVelocity() / 3, // ending velocity at point 2 from point 1
+                2 * configA.getMaxVelocity() / 3,
+                2 * configA.getMaxVelocity() / 3,
+                2 * configA.getMaxVelocity() / 3,
+                2 * configA.getMaxVelocity() / 3,
+                3 * configA.getMaxVelocity() / 4,
+                3 * configA.getMaxVelocity() / 4,
+                3 * configA.getMaxVelocity() / 4,
+                3 * configA.getMaxVelocity() / 4,
+                3 * configA.getMaxVelocity() / 4,
+                2 * configA.getMaxVelocity() / 3 };
+
+        for (int i = 0; i < waypoints.length - 1; i++) {
+            configA.setStartVelocity(startVelocities[i]);
+            configA.setEndVelocity(endVelocities[i]);
+
+            Trajectory trajectory = TrajectoryGenerator.generateTrajectory(waypoints[i],
+                    List.of(),
+                    waypoints[i + 1], // makes a path from one waypoint to the next
+                    configA); // makes sure that path fits under the constraints
+            var command = TrajectoryUtils.generateRamseteCommand(driveTrain, trajectory);
+            addCommands(command);
+        }
+    }
 }
