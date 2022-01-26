d0053acc71e1963e1629f298641d8d36d0da2d52
Jet Pham <jetthomaspham@gmail.com>
2022-01-25 17:39:49 -0800

    SpotlessJava check fixed

--- src/main/java/frc/robot/Constants.java
+++ src/main/java/frc/robot/Constants.java
@@ -14,15 +14,11 @@ import edu.wpi.first.math.system.plant.LinearSystemId;
 import edu.wpi.first.math.util.Units;
 
 /**
- * The Constants class provides a convenient place for teams to hold robot-wide
- * numerical or boolean
- * constants. This class should not be used for any other purpose. All constants
- * should be declared
+ * The Constants class provides a convenient place for teams to hold robot-wide numerical or boolean
+ * constants. This class should not be used for any other purpose. All constants should be declared
  * globally (i.e. public static). Do not put anything functional in this class.
  *
- * <p>
- * It is advised to statically import this class (or one of its inner classes)
- * wherever the
+ * <p>It is advised to statically import this class (or one of its inner classes) wherever the
  * constants are needed, to reduce verbosity.
  */
 public final class Constants {
@@ -60,14 +56,14 @@ public final class Constants {
   }
 
   public static final class DriveTrain {
-    public static final int[] kLeftEncoderPorts = new int[] { 10, 11 };
-    public static final int[] kRightEncoderPorts = new int[] { 12, 13 };
+    public static final int[] kLeftEncoderPorts = new int[] {10, 11};
+    public static final int[] kRightEncoderPorts = new int[] {12, 13};
     public static final boolean kLeftEncoderReversed = false;
     public static final boolean kRightEncoderReversed = true;
 
     public static final double kTrackWidthMeters = Units.inchesToMeters(21.5);
-    public static final DifferentialDriveKinematics kDriveKinematics = new DifferentialDriveKinematics(
-        kTrackWidthMeters);
+    public static final DifferentialDriveKinematics kDriveKinematics =
+        new DifferentialDriveKinematics(kTrackWidthMeters);
 
     public static final DCMotor kDriveGearbox = DCMotor.getFalcon500(2);
     public static final double kDriveGearing = 8.0;
@@ -93,11 +89,12 @@ public final class Constants {
     public static final double kvVoltSecondsPerRadian = 3.34;
     public static final double kaVoltSecondsSquaredPerRadian = 0.19;
 
-    public static final LinearSystem<N2, N2, N2> kDrivetrainPlant = LinearSystemId.identifyDrivetrainSystem(
-        kvVoltSecondsPerMeter,
-        kaVoltSecondsSquaredPerMeter,
-        kvVoltSecondsPerRadian,
-        kaVoltSecondsSquaredPerRadian);
+    public static final LinearSystem<N2, N2, N2> kDrivetrainPlant =
+        LinearSystemId.identifyDrivetrainSystem(
+            kvVoltSecondsPerMeter,
+            kaVoltSecondsSquaredPerMeter,
+            kvVoltSecondsPerRadian,
+            kaVoltSecondsSquaredPerRadian);
 
     public static final double kMaxVelocityMetersPerSecond = 2.0;
 
@@ -115,23 +112,23 @@ public final class Constants {
 
   public static final class Sim {
     public static final Pose2d[] redHubBallPos = {
-        new Pose2d(Units.inchesToMeters(438), Units.inchesToMeters(240), new Rotation2d()),
-        new Pose2d(Units.inchesToMeters(348), Units.inchesToMeters(297), new Rotation2d()),
-        new Pose2d(Units.inchesToMeters(245), Units.inchesToMeters(274), new Rotation2d()),
-        new Pose2d(Units.inchesToMeters(188), Units.inchesToMeters(132), new Rotation2d()),
-        new Pose2d(Units.inchesToMeters(355), Units.inchesToMeters(29), new Rotation2d()),
-        new Pose2d(Units.inchesToMeters(442), Units.inchesToMeters(89), new Rotation2d()),
-        new Pose2d(Units.inchesToMeters(580), Units.inchesToMeters(270), new Rotation2d())
+      new Pose2d(Units.inchesToMeters(438), Units.inchesToMeters(240), new Rotation2d()),
+      new Pose2d(Units.inchesToMeters(348), Units.inchesToMeters(297), new Rotation2d()),
+      new Pose2d(Units.inchesToMeters(245), Units.inchesToMeters(274), new Rotation2d()),
+      new Pose2d(Units.inchesToMeters(188), Units.inchesToMeters(132), new Rotation2d()),
+      new Pose2d(Units.inchesToMeters(355), Units.inchesToMeters(29), new Rotation2d()),
+      new Pose2d(Units.inchesToMeters(442), Units.inchesToMeters(89), new Rotation2d()),
+      new Pose2d(Units.inchesToMeters(580), Units.inchesToMeters(270), new Rotation2d())
     };
 
     public static final Pose2d[] blueHubBallPos = {
-        new Pose2d(Units.inchesToMeters(460), Units.inchesToMeters(192), new Rotation2d()),
-        new Pose2d(Units.inchesToMeters(294), Units.inchesToMeters(296), new Rotation2d()),
-        new Pose2d(Units.inchesToMeters(206), Units.inchesToMeters(235), new Rotation2d()),
-        new Pose2d(Units.inchesToMeters(210), Units.inchesToMeters(83), new Rotation2d()),
-        new Pose2d(Units.inchesToMeters(301), Units.inchesToMeters(27), new Rotation2d()),
-        new Pose2d(Units.inchesToMeters(405), Units.inchesToMeters(50), new Rotation2d()),
-        new Pose2d(Units.inchesToMeters(70), Units.inchesToMeters(55), new Rotation2d())
+      new Pose2d(Units.inchesToMeters(460), Units.inchesToMeters(192), new Rotation2d()),
+      new Pose2d(Units.inchesToMeters(294), Units.inchesToMeters(296), new Rotation2d()),
+      new Pose2d(Units.inchesToMeters(206), Units.inchesToMeters(235), new Rotation2d()),
+      new Pose2d(Units.inchesToMeters(210), Units.inchesToMeters(83), new Rotation2d()),
+      new Pose2d(Units.inchesToMeters(301), Units.inchesToMeters(27), new Rotation2d()),
+      new Pose2d(Units.inchesToMeters(405), Units.inchesToMeters(50), new Rotation2d()),
+      new Pose2d(Units.inchesToMeters(70), Units.inchesToMeters(55), new Rotation2d())
     };
 
     public static final double fieldWidthMeters = Units.feetToMeters(54);
@@ -143,8 +140,8 @@ public final class Constants {
     public static final double intakeLengthMeters = 0.3048;
     public static final double shotSpeedMetersPerSecond = 10;
 
-    public static final Pose2d hubPoseMeters = new Pose2d(12.557047, 7.275692,
-        new Rotation2d(Units.degreesToRadians(0)));
+    public static final Pose2d hubPoseMeters =
+        new Pose2d(12.557047, 7.275692, new Rotation2d(Units.degreesToRadians(0)));
     public static final Pose2d startPositionMeters = new Pose2d();
 
     public enum BallState {
--- src/main/java/frc/robot/commands/led/GetSubsystemStates.java
+++ src/main/java/frc/robot/commands/led/GetSubsystemStates.java
@@ -7,96 +7,93 @@ import edu.wpi.first.wpilibj.DriverStation;
 import edu.wpi.first.wpilibj2.command.CommandBase;
 import frc.robot.subsystems.*;
 
-/**
- * Sets the LED based on the subsystems' statuses
- */
+/** Sets the LED based on the subsystems' statuses */
 
 // TODO: rewrite LED subsystem with comments aswell as redesigning the assigned
 // colour values for each state
 
 public class GetSubsystemStates extends CommandBase {
-    @SuppressWarnings({"PMD.UnusedPrivateField", "PMD.SingularField"})
-    //TODO: make sure the correct subsystems are used
-    private final LED m_led;
-    private final Vision m_vision;
-    private final Intake m_intake;
-    private final Flywheel m_flywheel;
-    private final Climber m_climber ;
-    // private final Controls m_controls;
+  @SuppressWarnings({"PMD.UnusedPrivateField", "PMD.SingularField"})
+  // TODO: make sure the correct subsystems are used
+  private final LED m_led;
 
-    /**
-     * Sets the LED based on the subsystems' statuses
-     */
-    public GetSubsystemStates(LED led, Intake intake, Vision vision, Flywheel flywheel, Climber climber) {
-        m_led = led;
-        m_vision = vision;
-        m_climber = climber;
-        m_flywheel = flywheel;
-        m_intake = intake;
-        // Use addRequirements() here to declare subsystem dependencies.
-        addRequirements(led);
+  private final Vision m_vision;
+  private final Intake m_intake;
+  private final Flywheel m_flywheel;
+  private final Climber m_climber;
+  // private final Controls m_controls;
+
+  /** Sets the LED based on the subsystems' statuses */
+  public GetSubsystemStates(
+      LED led, Intake intake, Vision vision, Flywheel flywheel, Climber climber) {
+    m_led = led;
+    m_vision = vision;
+    m_climber = climber;
+    m_flywheel = flywheel;
+    m_intake = intake;
+    // Use addRequirements() here to declare subsystem dependencies.
+    addRequirements(led);
+  }
+
+  // Called when the command is initially scheduled.
+  @Override
+  public void initialize() {
+    m_led.expressState(LED.robotState.Idle);
+  }
+
+  // Called every time the scheduler runs while the command is scheduled.
+  @Override
+  public void execute() {
+    // the prioritized state to be expressed to the LEDs
+    // TODO: There should be disabled (not ready) and disabled (ready) like what we have on jango.
+    boolean Disabled = DriverStation.isDisabled();
+    boolean Idle = true; // Reevaluate what is "Idle"
+    boolean Intaking = m_intake.getIntakeState();
+    boolean VisionLock = m_vision.getGoalValidTarget();
+    boolean Shooting = true; // need a method for this
+    boolean Climbing = m_climber.getClimbState();
+
+    // set in order of priority to be expressed from the least priority to the highest priority
+    /*
+     Disabled,
+     Idle,
+     Intaking,
+     VisionLock,
+     Shooting,
+     Climbing
+    */
+    if (Disabled) {
+      m_led.expressState(LED.robotState.Disabled);
     }
-
-    // Called when the command is initially scheduled.
-    @Override
-    public void initialize() {
-        m_led.expressState(LED.robotState.Idle);
+    if (Idle) {
+      m_led.expressState(LED.robotState.Idle);
     }
-
-    // Called every time the scheduler runs while the command is scheduled.
-    @Override
-    public void execute() {
-        //the prioritized state to be expressed to the LEDs
-        //TODO: There should be disabled (not ready) and disabled (ready) like what we have on jango.
-        boolean Disabled = DriverStation.isDisabled();
-        boolean Idle = true; // Reevaluate what is "Idle"
-        boolean Intaking = m_intake.getIntakeState();
-        boolean VisionLock = m_vision.getGoalValidTarget();
-        boolean Shooting = true; // need a method for this
-        boolean Climbing = m_climber.getClimbState();
-
-        //set in order of priority to be expressed from the least priority to the highest priority
-        /*
-        Disabled,
-        Idle,
-        Intaking,
-        VisionLock,
-        Shooting,
-        Climbing
-       */
-        if(Disabled){
-            m_led.expressState(LED.robotState.Disabled);
-        }
-        if(Idle){
-            m_led.expressState(LED.robotState.Idle);
-        }
-        if(Intaking){
-            m_led.expressState(LED.robotState.Intaking);
-        }
-        if(VisionLock){
-            m_led.expressState(LED.robotState.VisionLock);
-        }
-        if(Shooting){
-            m_led.expressState(LED.robotState.Shooting);
-        }
-        if(Climbing){
-            m_led.expressState(LED.robotState.Climbing);
-        }
+    if (Intaking) {
+      m_led.expressState(LED.robotState.Intaking);
     }
-
-    // Called once the command ends or is interrupted.
-    @Override
-    public void end(boolean interrupted) {
+    if (VisionLock) {
+      m_led.expressState(LED.robotState.VisionLock);
     }
-
-    // Returns true when the command should end.
-    @Override
-    public boolean isFinished() {
-        return false;
+    if (Shooting) {
+      m_led.expressState(LED.robotState.Shooting);
     }
-
-    @Override
-    public boolean runsWhenDisabled() {
-        return true;
+    if (Climbing) {
+      m_led.expressState(LED.robotState.Climbing);
     }
+  }
+
+  // Called once the command ends or is interrupted.
+  @Override
+  public void end(boolean interrupted) {}
+
+  // Returns true when the command should end.
+  @Override
+  public boolean isFinished() {
+    return false;
+  }
+
+  @Override
+  public boolean runsWhenDisabled() {
+    return true;
+  }
 }
