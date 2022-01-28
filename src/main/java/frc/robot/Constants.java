5971e58fabff902ea852bca01197c0203cd7bef1
Benjamin Johnson <ben332004@gmail.com>
2022-01-28 09:50:59 -0800

    Better feedforward values

--- src/main/java/frc/robot/Constants.java
+++ src/main/java/frc/robot/Constants.java
@@ -156,17 +156,17 @@ public final class Constants {
 
     public static final int encoderUnitsPerRotation = 2048;
 
-    public static final double kFlywheelKs = 0.63348; // Jamgo: 0.53456;
+    public static final double kFlywheelKs = 0.53456;//0.63348; // Jamgo: 0.53456;
 
     // Volts per (radian per second)
-    public static final double kFlywheelKv = 0.15784; // Jamgo: 0.017092;
+    public static final double kFlywheelKv = 0.017092;//0.01;//0.15784; // Jamgo: 0.017092;
 
     // Volts per (radian per second squared)
-    public static final double kFlywheelKa = 0.034438; // Jamgo: 0.0083035;
+    public static final double kFlywheelKa = 0.0083035;//0.008;//0.034438; // Jamgo: 0.0083035;
 
     public static final double rpmTolerance = 25.0;
 
-    public static final double gearRatio = 2.0 / 3.0;
+    public static final double gearRatio = 1.0;//2.0 / 3.0;
   }
 
   public final class Turret {
