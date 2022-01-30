8be3edb6457955adc32d74d4cde57b88341af18a
Skye D <skyedouglas32@gmail.com>
2022-01-30 14:42:45 -0800

    Fixed Errors in Two Ball Auto
    Fixed Errors in Two Ball Auto

--- src/main/java/frc/robot/commands/auto/TwoBallAuto.java
+++ src/main/java/frc/robot/commands/auto/TwoBallAuto.java
@@ -3,12 +3,27 @@ package frc.robot.commands.auto;
 import com.pathplanner.lib.PathPlanner;
 import edu.wpi.first.math.trajectory.Trajectory;
 import edu.wpi.first.math.util.Units;
+import edu.wpi.first.wpilibj.RobotBase;
+import edu.wpi.first.wpilibj2.command.ConditionalCommand;
+import edu.wpi.first.wpilibj2.command.ParallelDeadlineGroup;
 import edu.wpi.first.wpilibj2.command.SequentialCommandGroup;
+import edu.wpi.first.wpilibj2.command.WaitCommand;
 import frc.robot.Constants.DriveTrain.DriveTrainNeutralMode;
 import frc.robot.commands.driveTrain.SetDriveTrainNeutralMode;
 import frc.robot.commands.driveTrain.SetOdometry;
+import frc.robot.commands.flywheel.SetAndHoldRpmSetpoint;
+import frc.robot.commands.indexer.RunIndexer;
+import frc.robot.commands.intake.IntakePiston;
+import frc.robot.commands.intake.RunIntake;
+import frc.robot.commands.turret.AutoUseVisionCorrection;
 import frc.robot.simulation.FieldSim;
+import frc.robot.simulation.SimulationShoot;
 import frc.robot.subsystems.DriveTrain;
+import frc.robot.subsystems.Flywheel;
+import frc.robot.subsystems.Indexer;
+import frc.robot.subsystems.Intake;
+import frc.robot.subsystems.Turret;
+import frc.robot.subsystems.Vision;
 import frc.vitruvianlib.utils.TrajectoryUtils;
 
 /** Intakes one cargo and shoots two cargo into the high goal. */
@@ -37,11 +52,11 @@ public class TwoBallAuto extends SequentialCommandGroup {
     // Stop (now with 2 cargo) and aim for high goal
     // Shoot 2 cargo into high goal
 
-    Trajectory trajectory =
+    Trajectory trajectory1 =
         PathPlanner.loadPath("TwoBallAuto", Units.feetToMeters(4), Units.feetToMeters(4), true);
 
-    VitruvianRamseteCommand command =
-        TrajectoryUtils.generateRamseteCommand(driveTrain, trajectory);
+    VitruvianRamseteCommand command1 =
+        TrajectoryUtils.generateRamseteCommand(driveTrain, trajectory1);
 
     Trajectory trajectory2 =
         PathPlanner.loadPath("TwoBallAuto-2", Units.feetToMeters(4), Units.feetToMeters(4), true);
@@ -56,22 +71,22 @@ public class TwoBallAuto extends SequentialCommandGroup {
      * End path
      */
     addCommands(
-        new SetOdometry(driveTrain, fieldSim, trajectory.getInitialPose()),
+        new SetOdometry(driveTrain, fieldSim, trajectory1.getInitialPose()),
         new SetDriveTrainNeutralMode(driveTrain, DriveTrainNeutralMode.HALF_BRAKE),
-        new SetIntakePiston(intake, true),
+        new IntakePiston(intake, true),
         new SetAndHoldRpmSetpoint(flywheel, vision, 3000),
         new ParallelDeadlineGroup(
             command1.andThen(() -> driveTrain.setMotorTankDrive(0, 0)),
-            new AutoControlledIntake(intake, indexer)
+            new RunIntake(intake, indexer)
             // TODO implement indexer
             ),
         new AutoUseVisionCorrection(turret, vision).withTimeout(0.25),
         new ConditionalCommand(new WaitCommand(0), new WaitCommand(0.5), flywheel::canShoot),
         // TODO how long does flywheel take to rev up? (should the flywheel run while
         // driving?)
-        new SetIntakePiston(intake, false),
+        new IntakePiston(intake, false),
         new ConditionalCommand(
-            new FeedAll(indexer),
+            new RunIndexer(indexer). withTimeout(1),
             new SimulationShoot(fieldSim, true).withTimeout(2),
             RobotBase::isReal),
         command2.andThen(() -> driveTrain.setMotorTankDrive(0, 0)));
