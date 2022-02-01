7a2eaad81619dc985b519dc0d1ec39ceb86f515b
benjaminJohnson2204 <68413232+benjaminJohnson2204@users.noreply.github.com>
2022-01-31 20:31:04 -0800

    Update Climber.java
    wat

--- src/main/java/frc/robot/subsystems/Climber.java
+++ src/main/java/frc/robot/subsystems/Climber.java
@@ -29,7 +29,7 @@ public class Climber extends SubsystemBase {
   /** Creates a new Climber. */
   public Climber() {
     // Set up climber motor
-    for (int i = 0; i < 1; i++) {
+    for (int i = 0; i < climbMotors.length; i++) {
       climbMotors[i].configFactoryDefault();
       climbMotors[i].setSelectedSensorPosition(0);
       climbMotors[i].setNeutralMode(NeutralMode.Brake);
