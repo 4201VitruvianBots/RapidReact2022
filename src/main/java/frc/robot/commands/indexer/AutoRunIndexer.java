8ec4e3edc213ca15abd882101c869758d169989d
Josiah Fu <josiah_fu@student.davincischools.org>
2022-02-20 16:54:15 -0800

    Tweaks to TwoBallAuto

---
+++ src/main/java/frc/robot/commands/indexer/AutoRunIndexer.java
@@ -0,0 +1,49 @@
+// Copyright (c) FIRST and other WPILib contributors.
+// Open Source Software; you can modify and/or share it under the terms of
+// the WPILib BSD license file in the root directory of this project.
+
+package frc.robot.commands.indexer;
+
+import edu.wpi.first.wpilibj2.command.CommandBase;
+import frc.robot.subsystems.Flywheel;
+import frc.robot.subsystems.Indexer;
+
+public class AutoRunIndexer extends CommandBase {
+
+  private final Indexer m_indexer;
+  private final Flywheel m_flywheel;
+  /** Creates a new RunIndexer. */
+  public AutoRunIndexer(Indexer indexer, Flywheel flywheel) {
+    m_indexer = indexer;
+    m_flywheel = flywheel;
+
+    // Use addRequirements() here to declare subsystem dependencies.
+    addRequirements(indexer);
+  }
+
+  // Called when the command is initially scheduled.
+  @Override
+  public void initialize() {}
+
+  // Called every time the scheduler runs while the command is scheduled.
+  @Override
+  public void execute() {
+    m_indexer.setIndexerPercentOutput(0.40);
+    if (m_flywheel.canShoot()) {
+      m_indexer.setKickerPercentOutput(0.40);
+    } else m_indexer.setKickerPercentOutput(0);
+  }
+
+  // Called once the command ends or is interrupted.
+  @Override
+  public void end(boolean interrupted) {
+    m_indexer.setIndexerPercentOutput(0);
+    m_indexer.setKickerPercentOutput(0);
+  }
+
+  // Returns true when the command should end.
+  @Override
+  public boolean isFinished() {
+    return false;
+  }
+}
