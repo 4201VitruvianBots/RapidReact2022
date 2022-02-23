0b081288adb8c938063ca25df996195d0d267747
Benjamin Johnson <benjamin_johnson@student.davincischools.org>
2022-02-23 09:16:59 -0800

    Fix flywheel & kicker speed

--- src/main/java/frc/robot/Constants.java
+++ src/main/java/frc/robot/Constants.java
@@ -201,7 +201,7 @@ public final class Constants {
     //    public static final double kFlywheelKv = 0.02001;
     //    public static final double kFlywheelKa = 0.002995;
     public static final double kFlywheelKs =
-        0.53456; // old flywheel: 0.53456; // 0.63348; // Jamgo: 0.53456;
+        0.73456; // old flywheel: 0.53456; // 0.63348; // Jamgo: 0.53456;
 
     // Volts per (radian per second)
     public static final double kFlywheelKv =
@@ -211,7 +211,7 @@ public final class Constants {
     public static final double kFlywheelKa =
         0.0026485; // old flywheel: 0.0083035; // 0.008;//0.034438; // Jamgo: 0.0083035;
 
-    public static final double rpmTolerance = 50.0;
+    public static final double rpmTolerance = 25.0;
 
     public static final double gearRatio = 1.0;
   }
