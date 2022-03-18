24919f2262a29d8537c3eab3d7da2d57feba46a3
JosiahFu <69983796+JosiahFu@users.noreply.github.com>
2022-03-17 22:42:53 -0700

    Remove unneeded todos One leak was fixed already, the others we can ignore

--- src/main/java/frc/robot/subsystems/Vision.java
+++ src/main/java/frc/robot/subsystems/Vision.java
@@ -124,7 +124,7 @@ public class Vision extends SubsystemBase {
   public double getTargetXAngle(CAMERA_POSITION position, int index) {
     switch (position) {
       case GOAL:
-        return goal_camera.getEntry("tx").getDouble(0); // TODO MEMORY LEAK
+        return goal_camera.getEntry("tx").getDouble(0);
       case INTAKE:
         if (getValidTarget(position)) {
           double[] nullValue = {-99};
