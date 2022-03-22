dd95228662f2e7e050bb539dbda2c110d4721bc7
Benjamin Johnson <benjamin_johnson@student.davincischools.org>
2022-03-22 15:16:36 -0700

    Clean up DriveToCargoTrajectory

--- src/main/java/frc/robot/commands/driveTrain/DriveToCargoTrajectory.java
+++ src/main/java/frc/robot/commands/driveTrain/DriveToCargoTrajectory.java
@@ -4,7 +4,6 @@ import edu.wpi.first.math.geometry.Pose2d;
 import edu.wpi.first.math.geometry.Rotation2d;
 import edu.wpi.first.math.geometry.Transform2d;
 import edu.wpi.first.math.geometry.Translation2d;
-import edu.wpi.first.math.kinematics.DifferentialDriveWheelSpeeds;
 import edu.wpi.first.math.trajectory.Trajectory;
 import edu.wpi.first.math.trajectory.TrajectoryConfig;
 import edu.wpi.first.math.trajectory.TrajectoryGenerator;
@@ -25,9 +24,15 @@ public class DriveToCargoTrajectory extends CommandBase {
 
   private final Vision m_vision;
 
-  private Trajectory m_path;
-  DifferentialDriveWheelSpeeds m_prevSpeed = new DifferentialDriveWheelSpeeds();
-  VitruvianRamseteCommand m_command;
+  private TrajectoryConfig m_pathConfig;
+  private Trajectory m_path, projectedPath;
+  private ArrayList<Pose2d> trajectoryStates;
+  private VitruvianRamseteCommand m_command;
+
+  private Pose2d m_defaultEndPose = new Pose2d();
+  private Pose2d startPos, endPos;
+  private Translation2d cargoPose;
+  private Rotation2d endAngle;
 
   /** Creates a new ExampleCommand. */
   public DriveToCargoTrajectory(DriveTrain driveTrain, Vision vision) {
@@ -38,48 +43,58 @@ public class DriveToCargoTrajectory extends CommandBase {
     addRequirements(driveTrain);
   }
 
+  public DriveToCargoTrajectory(DriveTrain driveTrain, Vision vision, Pose2d defaultEndPose) {
+    this(driveTrain, vision);
+    m_defaultEndPose = defaultEndPose;
+  }
+
   // Called when the command is initially scheduled.
   @Override
   public void initialize() {
+
+    startPos = m_driveTrain.getRobotPoseMeters();
+
     if (m_vision.getValidTarget(Constants.Vision.CAMERA_POSITION.INTAKE)) {
-      Pose2d startPos = m_driveTrain.getRobotPoseMeters();
+
       /** Field relative cargo pose */
-      Translation2d cargoPose = m_vision.getCargoPositionFieldAbsolute();
+      cargoPose = m_vision.getCargoPositionFieldAbsolute();
       /** Field relative cargo angle */
       // Rotation2d angleToCargo =
       //     new Rotation2d(cargoPose.getX() - startPos.getX(), cargoPose.getY() - startPos.getY());
-      Rotation2d endAngle = startPos.getRotation().unaryMinus();
-      Pose2d endPos =
+      endAngle = startPos.getRotation().unaryMinus();
+      endPos =
           new Pose2d(
               cargoPose.minus(Constants.Vision.INTAKE_TRANSLATION.rotateBy(endAngle)), endAngle);
-
-      TrajectoryConfig m_pathConfig =
-          new TrajectoryConfig(Units.feetToMeters(14), Units.feetToMeters(5))
-              // Add kinematics to ensure max speed is actually obeyed
-              .setReversed(true)
-              .setKinematics(Constants.DriveTrain.kDriveKinematics)
-              .setEndVelocity(0)
-              .setStartVelocity(0);
-
-      m_path = TrajectoryGenerator.generateTrajectory(startPos, List.of(), endPos, m_pathConfig);
-
-      Trajectory projectedPath =
-          m_path.transformBy(
-              new Transform2d(
-                  m_driveTrain.getRobotPoseMeters().getTranslation(),
-                  new Rotation2d(Units.degreesToRadians(m_driveTrain.getHeadingDegrees()))));
-
-      ArrayList<Pose2d> trajectoryStates = new ArrayList<Pose2d>();
-      trajectoryStates.addAll(
-          projectedPath.getStates().stream()
-              .map(state -> state.poseMeters)
-              .collect(Collectors.toList()));
-
-      m_driveTrain.setCurrentTrajectory(m_path);
-
-      m_command = TrajectoryUtils.generateRamseteCommand(m_driveTrain, m_path);
-      m_command.initialize();
+    } else {
+      endPos = m_defaultEndPose;
     }
+
+    m_pathConfig =
+        new TrajectoryConfig(Units.feetToMeters(14), Units.feetToMeters(5))
+            // Add kinematics to ensure max speed is actually obeyed
+            .setReversed(true)
+            .setKinematics(Constants.DriveTrain.kDriveKinematics)
+            .setEndVelocity(0)
+            .setStartVelocity(0);
+
+    m_path = TrajectoryGenerator.generateTrajectory(startPos, List.of(), endPos, m_pathConfig);
+
+    projectedPath =
+        m_path.transformBy(
+            new Transform2d(
+                m_driveTrain.getRobotPoseMeters().getTranslation(),
+                new Rotation2d(Units.degreesToRadians(m_driveTrain.getHeadingDegrees()))));
+
+    trajectoryStates = new ArrayList<Pose2d>();
+    trajectoryStates.addAll(
+        projectedPath.getStates().stream()
+            .map(state -> state.poseMeters)
+            .collect(Collectors.toList()));
+
+    m_driveTrain.setCurrentTrajectory(m_path);
+
+    m_command = TrajectoryUtils.generateRamseteCommand(m_driveTrain, m_path);
+    m_command.initialize();
   }
 
   // Called every time the scheduler runs while the command is scheduled.
