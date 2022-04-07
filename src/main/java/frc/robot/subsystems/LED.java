18ff54835767a92235d697c42ee65bd32168af2e
Benjamin Johnson <benjamin_johnson@student.davincischools.org>
2022-04-07 08:53:10 -0700

    Fix intaking LED state

--- src/main/java/frc/robot/subsystems/LED.java
+++ src/main/java/frc/robot/subsystems/LED.java
@@ -103,7 +103,7 @@ public class LED extends SubsystemBase {
     if (state != currentRobotState) {
       switch (state) {
         case Intaking: // Strobing Yellow
-          setPattern(255, 255, 0, 0, 0.1, AnimationTypes.Solid);
+          setPattern(255, 128, 0, 0, 0, AnimationTypes.Solid);
           break;
         case Enabled: // Solid Green
           setPattern(0, 255, 0, 0, 0, AnimationTypes.Solid);
